1ec662d0782eb670d120f5a91baece358e9ef32e
Grigory <grishka5556@gmail.com>
2023-01-10 19:13:48 +0300

    first commit

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,61 @@
+import project01 from "./../img/projects/01.jpg"
+import project01Big from "./../img/projects/01-big.jpg"
+
+import project02 from "./../img/projects/02.jpg"
+import project02Big from "./../img/projects/02-big.jpg"
+
+import project03 from "./../img/projects/03.jpg"
+import project03Big from "./../img/projects/03-big.jpg"
+
+import project04 from "./../img/projects/04.jpg"
+import project04Big from "./../img/projects/04-big.jpg"
+
+import project05 from "./../img/projects/05.jpg"
+import project05Big from "./../img/projects/05-big.jpg"
+
+import project06 from "./../img/projects/06.jpg"
+import project06Big from "./../img/projects/06-big.jpg"
+
+const projects = [
+    {
+        title: "Travel agency website",
+        skills: "JavaScript, HTML, CSS",
+        img: project01,
+        imgBig: project01Big,
+        gitHubLink: "https://github.com/GrigoriyBerezin/YourTour.git",
+    },
+    {
+        title: "Programmer's Blog",
+        img: project02,
+        imgBig: project02Big,
+        skills: "React, Node.js, MongoDB, TailwindCSS",
+        gitHubLink: "https://github.com",
+    },
+    {
+        title: "ToDo APP",
+        img: project03,
+        imgBig: project03Big,
+        skills: "JavaScript, HTML, CSS",
+        gitHubLink: "https://github.com",
+    },
+    {
+        title: "Dating app",
+        img: project04,
+        imgBig: project04Big,
+        skills: "In the process of developing",
+    },
+    {
+        title: "Landing",
+        img: project05,
+        imgBig: project05Big,
+        skills: "In the process of developing",
+    },
+    {
+        title: "Gaming community",
+        img: project06,
+        imgBig: project06Big,
+        skills: "In the process of developing",
+    },
+]
+
+export { projects }
