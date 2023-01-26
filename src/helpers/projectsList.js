ec555fc4faab7e945b0e6c34ba149fdca78ee9b7
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-26 09:46:24 +0300

    new project add in portfolio

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,28 +1,29 @@
 import project01 from "./../img/projects/01.jpg"
 import project01Big from "./../img/projects/01-big.jpg"
 
-import project02 from "./../img/projects/02.jpg"
-import project02Big from "./../img/projects/02-big.jpg"
+import project02 from "./../img/projects/1.png"
+import project02Big from "./../img/projects/1.png"
 
-import project03 from "./../img/projects/03.jpg"
-import project03Big from "./../img/projects/03-big.jpg"
+import project03 from "./../img/projects/3.png"
+import project03Big from "./../img/projects/3.png"
 
-import project04 from "./../img/projects/04.jpg"
-import project04Big from "./../img/projects/04-big.jpg"
+import project04 from "./../img/projects/4.png"
+import project04Big from "./../img/projects/4.png"
 
-import project05 from "./../img/projects/05.jpg"
-import project05Big from "./../img/projects/05-big.jpg"
+import project05 from "./../img/projects/5.png"
+import project05Big from "./../img/projects/5.png"
 
-import project06 from "./../img/projects/06.jpg"
-import project06Big from "./../img/projects/06-big.jpg"
+import project06 from "./../img/projects/2.png"
+import project06Big from "./../img/projects/2.png"
 
 const projects = [
     {
+
         title: "Travel agency website",
         skills: "JavaScript, HTML, CSS",
         img: project01,
         imgBig: project01Big,
-        gitHubLink: "https://github.com/GrigoriyBerezin/YourTour.git",
+        gitHubLink: "https://github.com/gregorybv/YourTour.git",
     },
     {
         title: "Programmer's Blog",
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,6 +1,6 @@
 import {useParams} from "react-router-dom";
 import BtnGitHub from "../components/btnGitHub/BtnGitHub";
-import {projects} from "./../helpers/projectsList"
+import {projects} from "../helpers/projectsList"
 
 const Project = () => {
   const {id} = useParams();
