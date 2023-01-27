3c09f4c1ae55ab57a23091bc65dbf8b89f2c84bd
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-27 11:53:26 +0300

    new home page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import "./styles/main.css";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import "./styles/main.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
-import Footer from "./components/footer/Footer";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Project from "./pages/Project";
-import Contacts from "./pages/Contacts";
+import Footer from "./components/footer/Footer"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Project from "./pages/Project"
+import Contacts from "./pages/Contacts"
 import ScrollToTop from "./utils/scrollToTop"
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <ScrollToTop/>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/projects" element={<Projects/>}/>
-          <Route path="/project/:id" element={<Project/>}/>
-          <Route path="/contacts" element={<Contacts/>}/>
-        </Routes>
-        <Footer/>
-      </Router>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                <ScrollToTop />
+                <Navbar />
+                <Routes>
+                    <Route path='/react-portfolio' element={<Home />} />
+                    <Route path='/projects' element={<Projects />} />
+                    <Route path='/project/:id' element={<Project />} />
+                    <Route path='/contacts' element={<Contacts />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
